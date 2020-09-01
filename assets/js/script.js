79ddf9d7f2b6e4571e3fdf3af8fa2259b1cf093f
Jaquelin Rocha <jaquir16@gmail.com>
2020-08-31 23:07:47 -0700

    added all new files

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// collapseable menu jquery code
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
+
+// initialize parallax  code
+
+$(document).ready(function () {
+  $(".parallax").parallax();
+});
